d65a104ff1ebe59308fefad1ea9b4e4ae85ba253
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2024-08-29 08:38:46 +0200

    chore: adds tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,19 +10,6 @@ pub struct Attribute {
     pub value: String,
 }
 
-pub fn get_row_vec(row: &str, delimiter: &str, num_cols: usize) -> Vec<String> {
-    let mut items: Vec<&str> = row.split(delimiter).collect();
-    if items.len() < num_cols {
-        for _ in 0..num_cols - items.len() {
-            items.push("")
-        }
-    }
-
-    let items_slice = &items[0..num_cols];
-
-    items_slice.iter().map(|s| s.trim().to_string()).collect()
-}
-
 #[derive(Debug, ValueEnum, Clone)]
 pub enum TableStyle {
     Ascii,
@@ -70,6 +57,19 @@ impl TableStyle {
     }
 }
 
+pub fn get_row_vec(row: &str, delimiter: &str, num_cols: usize) -> Vec<String> {
+    let mut items: Vec<&str> = row.split(delimiter).collect();
+    if items.len() < num_cols {
+        for _ in 0..num_cols - items.len() {
+            items.push("")
+        }
+    }
+
+    let items_slice = &items[0..num_cols];
+
+    items_slice.iter().map(|s| s.trim().to_string()).collect()
+}
+
 pub fn get_output(data: &[Vec<String>], config: RenderConfig) -> String {
     let mut builder = Builder::default();
 
@@ -91,4 +91,182 @@ pub fn get_output(data: &[Vec<String>], config: RenderConfig) -> String {
     config.style.apply_to(&mut b);
 
     b.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn generate_data() -> Vec<Vec<String>> {
+        let data = vec![
+            ["row1col1", "row1col2", "row1col3"],
+            ["row2col1", "row2col2", "row2col3"],
+            ["row3col1", "row3col2", "row3col3"],
+        ];
+        data.into_iter()
+            .map(|row| {
+                row.into_iter()
+                    .map(|item| item.to_string())
+                    .collect::<Vec<String>>()
+            })
+            .collect::<Vec<Vec<String>>>()
+    }
+
+    #[test]
+    fn get_row_vec_works_with_defaults() {
+        // GIVEN
+        let row_data = "row1col1,row1col2,row1col3";
+
+        // WHEN
+        let got = get_row_vec(row_data, ",", 3);
+
+        // THEN
+        let expected = ["row1col1", "row1col2", "row1col3"]
+            .into_iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<String>>();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_row_vec_works_with_custom_delimiter() {
+        // GIVEN
+        let row_data = "row1col1|row1col2|row1col3";
+
+        // WHEN
+        let got = get_row_vec(row_data, "|", 3);
+
+        // THEN
+        let expected = ["row1col1", "row1col2", "row1col3"]
+            .into_iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<String>>();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_row_vec_trims_cells() {
+        // GIVEN
+        let row_data = "row1col1 , row1col2,    row1col3   ";
+
+        // WHEN
+        let got = get_row_vec(row_data, ",", 3);
+
+        // THEN
+        let expected = ["row1col1", "row1col2", "row1col3"]
+            .into_iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<String>>();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_row_vec_limits_output_to_num_cols() {
+        // GIVEN
+        let row_data = "row1col1,row1col2,row1col3";
+
+        // WHEN
+        let got = get_row_vec(row_data, ",", 2);
+
+        // THEN
+        let expected = ["row1col1", "row1col2"]
+            .into_iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<String>>();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_row_vec_creates_empty_cells_if_asked() {
+        // GIVEN
+        let row_data = "row1col1,row1col2,row1col3";
+
+        // WHEN
+        let got = get_row_vec(row_data, ",", 5);
+
+        // THEN
+        let expected = ["row1col1", "row1col2", "row1col3", "", ""]
+            .into_iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<String>>();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn renders_defaults_correctly() {
+        // GIVEN
+        let data = generate_data();
+        let config = RenderConfig {
+            style: TableStyle::Sharp,
+            padding: TablePadding { left: 1, right: 1 },
+        };
+
+        // WHEN
+        let got = get_output(&data, config);
+
+        // THEN
+        let expected = "
+┌──────────┬──────────┬──────────┐
+│ row1col1 │ row1col2 │ row1col3 │
+├──────────┼──────────┼──────────┤
+│ row2col1 │ row2col2 │ row2col3 │
+│ row3col1 │ row3col2 │ row3col3 │
+└──────────┴──────────┴──────────┘
+";
+        assert_eq!(got, expected.trim());
+    }
+
+    #[test]
+    fn renders_ascii_style_correctly() {
+        // GIVEN
+        let data = generate_data();
+        let config = RenderConfig {
+            style: TableStyle::Ascii,
+            padding: TablePadding { left: 1, right: 1 },
+        };
+
+        // WHEN
+        let got = get_output(&data, config);
+
+        // THEN
+        let expected = "
++----------+----------+----------+
+| row1col1 | row1col2 | row1col3 |
++----------+----------+----------+
+| row2col1 | row2col2 | row2col3 |
++----------+----------+----------+
+| row3col1 | row3col2 | row3col3 |
++----------+----------+----------+
+";
+        assert_eq!(got, expected.trim());
+    }
+
+    #[test]
+    fn renders_padding_correctly() {
+        // GIVEN
+        let data = generate_data();
+        let config = RenderConfig {
+            style: TableStyle::Sharp,
+            padding: TablePadding { left: 1, right: 2 },
+        };
+
+        // WHEN
+        let got = get_output(&data, config);
+
+        // THEN
+        let expected = "
+┌───────────┬───────────┬───────────┐
+│ row1col1  │ row1col2  │ row1col3  │
+├───────────┼───────────┼───────────┤
+│ row2col1  │ row2col2  │ row2col3  │
+│ row3col1  │ row3col2  │ row3col3  │
+└───────────┴───────────┴───────────┘
+";
+        assert_eq!(got, expected.trim());
+    }
 }
