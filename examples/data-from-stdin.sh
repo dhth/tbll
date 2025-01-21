1f6ac091a29c0648f5b9868c47938c5c41274c86
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-01-21 18:51:10 +0100

    test: add integration tests

--- examples/data-from-stdin.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/env bash
-
-cat ./examples/movies.txt | tbll "$@" \
-    -s \
-    --headers 'Title,Year,Director,Genre'
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod config;
 mod output;
 
+use anyhow::Context;
 use clap::Parser;
 use config::{RenderConfig, TablePadding, TableStyle};
 use output::{get_output, get_row_vec};
-use std::io::{self, BufRead};
-use std::process;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 const ROW_DELIMITER: &str = ",";
 
@@ -16,7 +17,10 @@ struct Args {
     /// Row elements
     #[arg(short = 'r', long = "row", value_name = "STRING:::STRING...")]
     row: Option<Vec<String>>,
-    /// Whether to read row elements from stdin
+    /// Input file path
+    #[arg(short = 'p', long = "input-path", value_name = "STRING")]
+    input_file_path: Option<String>,
+    /// Whether to read input from stdin
     #[arg(short = 's', long = "read-stdin")]
     read_stdin: bool,
     /// Delimiter to use
@@ -44,13 +48,12 @@ struct Args {
     right_pad: usize,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     let mut data: Vec<Vec<String>> = Vec::new();
 
     let mut num_cols = args.num_cols;
-    let style = args.style;
 
     if let Some(headers) = args.headers {
         let headers_vec: Vec<&str> = headers.split(",").collect();
@@ -70,27 +73,50 @@ fn main() {
         }
     }
 
-    if args.read_stdin {
-        let stdin = io::stdin();
-        for line in stdin.lock().lines() {
-            let line = line.unwrap_or_else(|err| {
-                eprintln!("Error: could not read line from stdin: {}", err);
-                process::exit(1);
-            });
-
-            let attr = get_row_vec(&line, &args.delimiter, num_cols);
-            data.push(attr)
+    match (args.read_stdin, args.input_file_path) {
+        (false, None) => {
+            return Err(anyhow::anyhow!(
+                "a source needs to be provided (either a file or stdin)"
+            ));
         }
-    }
+        (true, Some(_)) => {
+            return Err(anyhow::anyhow!(
+                "only one source (either a file or stdin) can be used at a time"
+            ));
+        }
+        (true, None) => {
+            let stdin = io::stdin();
+            for line in stdin.lock().lines() {
+                let line = line.context("couldn't read line from stdin")?;
+                let attr = get_row_vec(&line, &args.delimiter, num_cols);
+                data.push(attr)
+            }
+        }
+        (false, Some(path)) => {
+            let file = File::open(path)?;
+            let reader = BufReader::new(file);
+
+            for line in reader.lines() {
+                let line = line.context("couldn't read line from stdin")?;
+                let attr = get_row_vec(&line, &args.delimiter, num_cols);
+                data.push(attr)
+            }
+        }
+    };
 
     let padding = TablePadding {
         left: args.left_pad,
         right: args.right_pad,
     };
 
-    let config = RenderConfig { style, padding };
+    let config = RenderConfig {
+        style: args.style,
+        padding,
+    };
 
     let output = get_output(&data, config);
 
     println!("{output}");
+
+    Ok(())
 }
