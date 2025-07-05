e7c7d4406c38ec4826a1e8a64613e32e684cdfbb
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-07-05 17:22:51 +0200

    improve testing (#39)

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,24 @@
 use insta_cmd::get_cargo_bin;
-use std::process::Command;
+use std::{ffi::OsStr, path::PathBuf, process::Command};
 
-pub fn base_command() -> Command {
-    Command::new(get_cargo_bin("tbll"))
+pub struct Fixture {
+    bin_path: PathBuf,
+}
+
+impl Fixture {
+    pub fn new() -> Self {
+        let bin_path = get_cargo_bin("tbll");
+
+        Self { bin_path }
+    }
+
+    pub fn cmd<I, S>(&self, args: I) -> Command
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut command = Command::new(&self.bin_path);
+        command.args(args);
+        command
+    }
 }
